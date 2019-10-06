299350462269dfa3d1f5818615f34df058577968
Maciej Sadoś <rubik@pjwstk.edu.pl>
2019-10-06 20:05:55 +0200

    Dodano zadanie 5 z ćwiczeń 1

---
+++ zad5.scala
@@ -0,0 +1,12 @@
+object HelloWorld {
+  def main(args: Array[String]) : Unit = {
+    val itemsPrices = Map("czekolada" -> 5.00, "jablko" -> 1.00, "komputer" -> 6000.00)
+    
+    val itemsPricesAfterDiscount = getItemsWithTenPercentDiscount(itemsPrices);
+    println(itemsPricesAfterDiscount)
+  }
+  
+  def getItemsWithTenPercentDiscount(itemsPrices: Map[String, Double]) : Map[String, Double] = {
+    itemsPrices.map(kv => (kv._1, 0.9*kv._2))
+  }
+}
