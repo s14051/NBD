9ec53bdf4c4b512711a3ae996241f184c2aedbd5
Maciej Sadoś <rubik@pjwstk.edu.pl>
2019-10-06 20:10:32 +0200

    Dodano zadanie 6 z ćwiczeń 1

---
+++ zad6.scala
@@ -0,0 +1,10 @@
+object HelloWorld {
+  def main(args: Array[String]) : Unit = {
+    val tuple = ("jakiś string", 22, true)
+    printTuple(tuple)
+  }
+  
+  def printTuple(tuple : Tuple3[String, Int, Boolean]) : Unit = {
+    println(tuple._1 + ", " + tuple._2 + ", " + tuple._3)
+  }
+}
