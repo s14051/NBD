f941b14174916a1183a613e85b9912063354e0b4
Maciej Sadoś <rubik@pjwstk.edu.pl>
2019-10-06 20:24:37 +0200

    Dodano zadanie 7 z ćwiczeń 1

---
+++ zad7.scala
@@ -0,0 +1,12 @@
+object HelloWorld {
+  def main(args: Array[String]) : Unit = {
+    val itemsPrices = Map("czekolada" -> 5.00, "jablko" -> 1.00, "komputer" -> 6000.00)
+
+    println(searchValueByKeyInMap[String, Double](itemsPrices, "komputer"))
+    println(searchValueByKeyInMap[String, Double](itemsPrices, "asdf"))
+  }
+  
+  def searchValueByKeyInMap[K,V](map : Map[K, V], key : K) : Any = {
+    map.get(key).getOrElse("Podany klucz nie istnieje w dostarczonej mapie")
+  }
+}
