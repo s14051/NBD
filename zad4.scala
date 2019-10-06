49b4ef5a1b495878355172c09021ce824e85e15a
Maciej Sadoś <rubik@pjwstk.edu.pl>
2019-10-06 19:50:47 +0200

    Dodano zadanie 4 z ćwiczeń 1

---
+++ zad4.scala
@@ -0,0 +1,28 @@
+object HelloWorld {
+  def main(args: Array[String]) : Unit = {
+    val daysOfWeek = List(
+      "poniedziałek", 
+      "wtorek",
+      "środa",
+      "czwartek",
+      "piątek",
+      "sobota",
+      "niedziela")
+    
+    println(makeCommaSeparatedStringUsingFoldl(daysOfWeek))
+    println(makeCommaSeparatedStringUsingFoldr(daysOfWeek))
+    println(makeCommaSeparatedStringUsingFoldlStartWithP(daysOfWeek))
+  }
+  
+  def makeCommaSeparatedStringUsingFoldl(list: List[String]) : String = {
+    list.tail.foldLeft(list.head)(_ + ", " + _)
+  }
+  
+  def makeCommaSeparatedStringUsingFoldr(list: List[String]) : String = {
+    list.dropRight(1).foldRight(list.last)(_ + ", " + _)
+  }
+  
+  def makeCommaSeparatedStringUsingFoldlStartWithP(list: List[String]) : String = {
+    list.filter(_.toUpperCase().startsWith("P")).tail.foldLeft(list.head)(_ + ", " + _)
+  }
+}
