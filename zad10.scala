49c0eec22d92abc25fec757799e1301690151edb
Maciej Sadoś <rubik@pjwstk.edu.pl>
2019-10-06 22:15:52 +0200

    Dodano zadanie 10 z ćwiczeń 1

---
+++ zad10.scala
@@ -0,0 +1,13 @@
+object HelloWorld {
+  def main(args: Array[String]) : Unit = {
+    val realNumbers = List(1.0, -2.0, -5.0, 10.0, -44.0, 0.0, 22.0, 12.0, -22.0, 13.0, 5.0)
+
+    println(getAbsoluteValues(realNumbers))
+  }
+  
+  def getAbsoluteValues(list: List[Double]) : List[Double] = {
+    val isBetweenMinusFiveAndTwelveInclusive : Double => Boolean = v => v >= -5.0 && v <= 12.0;
+    
+    list.filter(isBetweenMinusFiveAndTwelveInclusive).map(_.abs):::list.filter(!isBetweenMinusFiveAndTwelveInclusive(_))
+  }
+}
