e6318dc745ac2acd28bc25c8eba23c9052798fab
Maciej Sadoś <rubik@pjwstk.edu.pl>
2019-10-06 18:52:28 +0200

    Dodano zadanie 2 z ćwiczeń 1

---
+++ zad2.scala
@@ -0,0 +1,28 @@
+object HelloWorld {
+  def main(args: Array[String]) : Unit = {
+    val daysOfWeek = List(
+      "poniedziałek", 
+      "wtorek",
+      "środa",
+      "czwartek",
+      "piątek",
+      "sobota",
+      "niedziela")
+    
+    println(makeReversedCommaSeparatedStringFrom(daysOfWeek))
+  }
+  
+  def makeCommaSeparatedStringFrom(list: List[String]) : String = {
+    if (!list.tail.isEmpty)
+      list.head + ", " + makeCommaSeparatedStringFrom(list.tail)
+    else 
+      list.head
+  }
+  
+  def makeReversedCommaSeparatedStringFrom(list: List[String]) : String = {
+    if (!list.tail.isEmpty)
+      makeReversedCommaSeparatedStringFrom(list.tail) + ", " + list.head
+    else 
+      list.head
+  }
+}
