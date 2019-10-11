41034e3b7e93aa99f57b1fa99e084f3ffa4c3d76
Maciej Sadoś <rubik@pjwstk.edu.pl>
2019-10-11 13:11:32 +0200

    Dodano zadanie 1 z ćwiczeń 4

---
+++ Cwiczenia_4/Zadanie 1/zapytanie_1_A.js
@@ -0,0 +1,11 @@
+printjson(
+    db.people.aggregate( 
+        { 
+            $group: {
+                _id: "$sex",
+                "średnia waga": { $avg: { $toDouble: "$weight" } },
+                "średni wzrost": { $avg: { $toDouble: "$height" } }
+            }
+        })
+    .toArray()
+);
